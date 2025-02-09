4f54e37a0f1fc5eea1a60b211474fb248514cde8
sudo-batman <gauravrai7860@gmail.com>
2025-02-09 15:02:10 +0530

    Create 2_merge_two_sorted_linked_lists.py

---
+++ LinkedList/2_merge_two_sorted_linked_lists.py
@@ -0,0 +1,29 @@
+# https://neetcode.io/problems/merge-two-sorted-linked-lists
+# You are given the heads of two sorted linked lists list1 and list2.
+# Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+# The new list should be made up of nodes from list1 and list2.
+
+# Input: list1 = [1,2,4], list2 = [1,3,5]
+# Output: [1,1,2,3,4,5]
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = node = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+        node.next = list1 or list2
+
+        return dummy.next
