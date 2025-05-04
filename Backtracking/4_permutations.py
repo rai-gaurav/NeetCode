58ee48b5746a98e2136d435f2a4c14043859b864
rai-gaurav <gauravrai7860@gmail.com>
2025-05-04 23:19:40 +0530

    Create 4_permutations.py

---
+++ Backtracking/4_permutations.py
@@ -0,0 +1,20 @@
+# https://neetcode.io/problems/permutations
+# Given an array nums of unique integers, return all the possible permutations. You may return the answer in any order.
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+        
+        perms = self.permute(nums[1:])
+        res = []
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+
+        return res
