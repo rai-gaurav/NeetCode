294bb7c8fc6cee827c391b8523c89e63426439d1
rai-gaurav <gauravrai7860@gmail.com>
2025-05-05 11:18:38 +0530

    Create 2_number_of_one_bits.py

---
+++ BitManipulation/2_number_of_one_bits.py
@@ -0,0 +1,16 @@
+# https://neetcode.io/problems/number-of-one-bits
+
+# You are given an unsigned integer n. Return the number of 1 bits in its binary representation.
+# You may assume n is a non-negative integer which fits within 32-bits.
+
+# Input: n = 00000000000000000000000000010111
+# Output: 4
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            # res += 1 if n & 1 else 0
+            res += n % 2
+            n = n >> 1
+        return res
